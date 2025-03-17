57a9a8c0996350416107477a6adaa64185df27ae
IlVez23 <andreaverba@gmail.com>
2025-03-17 20:54:11 +0100

    added file

---
+++ test_streamlit.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import numpy as np
+
+st.title("Hello, Streamlit! 👋")
+st.write("This is your first web app in Python!")
+
+if st.button("Click me!"):
+    st.write("Button clicked!")
