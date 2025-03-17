572b6871ac3695a01ce0db9f729a1f31e8e50dfe
IlVez23 <andreaverba@gmail.com>
2025-03-17 21:48:10 +0100

    added dataframe

--- test_streamlit.py
+++ test_streamlit.py
@@ -1,8 +1,21 @@
 import streamlit as st
 import numpy as np
+import pandas as pd
 
 st.title("Hello, Streamlit! 👋")
 st.write("This is your first web app in Python!")
 
 if st.button("Click me!"):
     st.write("Button clicked!")
+
+
+
+
+st.write("Let´s make teh first dataset")
+
+df = pd.DataFrame({
+    'name': ['ciaone', 'scoreggione', 'blabla'],
+    'valori':[12, 34, 76]
+})
+
+st.dataframe(df)
